0d20491d37cdd3bb0aa94e9ed740df2886657449
Ana <alcvlara@sga.pucminas.br>
2019-02-13 15:20:42 -0200

    adding list

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Header from "./components/header";
 import Main from "./pages/main";
 import "./styles.css";
 
-// const App = () => {
-//   <div className="App">
-//       <Header />
-//   </div>
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Header />
+//         <Main />
+//       </div>
+//     );
+//   }
 // }
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
-    );
-  }
-}
 
 export default App;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,34 @@
-import React, { Components } from "react";
+import React, { Component } from "react";
 import api from "../../services/api";
 
-export default class Main extends Components {
+export default class Main extends Component {
+  state = {
+    products: []
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get("/products");
+
+    this.setState({ products: response.data.docs });
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    const { products } = this.state;
+
+    return (
+      <div className="product-list">
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            <a href="">Acessar</a>
+          </article>
+        ))}
+      </div>
+    );
   }
 }
